a4a1ed8edb5ac955862f8e7dcba9b11d3016fc9e
Juan Loaiza <juanmaloaiza@gmail.com>
2023-04-14 23:10:34 -0500

    Emision inicial

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.sql.*;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
+
---
+++ src/main/resources/data-postgresql.sql
@@ -0,0 +1,4 @@
+delete from customer;
+insert into customer (name,age) values ('Joe Psql', 88);
+insert into customer (name,age) values ('Jack Psql', 54);
+insert into customer (name,age) values ('Ann Psql', 32);
